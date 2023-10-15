0e702a60af7ae51f979b5edcc9632ff954b86ee5
Jacob Young <youngjacob2002@gmail.com>
2023-10-14 18:06:14 -0700

    asdjaskd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import React, {useState} from 'react';
+import { addDoc, collection } from "@firebase/firestore"
+import { firestore } from "./firebaseSetup/firebase"
 
 function App() {
+  const [food, setFood] = useState("");
+  const API_KEY = '090ec85dd264d9e6f2affec5165e7e7e';
+
+  const runQuery = () => {
+    axios.get('https://trackapi.nutritionix.com/v2/search/instant?query=' + {food} + "limit=1", {
+      headers: {
+        'x-app-id': 'ab53e741',
+        'x-app-key': API_KEY, 
+      },
+    }).then((response) => {
+        // Handle the response data here
+        console.log('Response data:', response.data);
+    }).catch((error) => {
+        // Handle any errors here
+        console.error('Error:', error);
+    });
+  }
+
+  const writeToFB = () => {
+    const ref = collection(firestore, "test_data") // Firebase creates this automatically
+ 
+    let data = {
+        testData: "sandnakndjkasndjkasknjd"
+    }
+    
+    try {
+        addDoc(ref, data)
+    } catch(err) {
+        console.log(err)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type='text' onChange={(e) => setFood(e.target.value)}></input>
+      <button onClick={()=>runQuery()}>Get Calories</button>
+
+      <button>Search with Picture</button>
     </div>
   );
 }
