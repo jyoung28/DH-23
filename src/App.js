814e4fbb0c70de1adb7b9b0d9b449cb9f7d6deee
MarkUnivWash <142341421+MarkUnivWash@users.noreply.github.com>
2023-10-14 20:07:58 -0700

    Camera PAGE

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Home from './components/Home';
 import GoalPage from './components/GoalPage';
 import SearchPage from './components/SearchPage';
 import ProfilePage from './components/ProfilePage';
+import CameraPage from './components/CameraPage';
 import {HashRouter, Route, Routes} from 'react-router-dom'
 
 import BottomNavbar from './components/bottomNavbar'
@@ -22,6 +23,7 @@ function App() {
         <HashRouter basename='/'>
             {/* have the header only be on pages after login */}
             {/* <Header/> */}
+         
             <Routes>
                 <Route element={<LoginPage/>} path="/" exact/>
                 <Route element={<GoalPage/>} path="/goal" exact/>
@@ -32,9 +34,10 @@ function App() {
                 <Route element={<Projects/>} path="/projects" exact/>
                 <Route element={<ContactMe/>} path="/contactme" exact/> */}
             </Routes>
+            <BottomNavbar></BottomNavbar>
         </HashRouter>
     </div>
-             
+             <CameraPage></CameraPage>
     </ThemeProvider>
   );
 }
--- src/components/CameraPage.js
+++ src/components/CameraPage.js
@@ -1,8 +1,49 @@
-import React from "react";
+import React, { useState } from 'react';
+import Webcam from 'react-webcam';
+import { Button } from '@mui/material';
 
+const CameraPage = ({ onSaveImage }) => {
+  const webcamRef = React.useRef(null);
+  const [image, setImage] = useState(null);
+  const [capturing, setCapturing] = useState(false);
 
-const CameraPage = () => {
-    return <div></div>
-}
+  const capture = () => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImage(imageSrc);
+    setCapturing(false);
+  };
+
+  const retake = () => {
+    setImage(null);
+    setCapturing(true);
+  };
+
+  const saveImage = () => {
+    // Save the image to local storage or Firebase here
+    onSaveImage(image);
+  };
+
+  return (
+    <div>
+      {capturing ? (
+        <>
+          <Webcam
+            audio={false}
+            height={400}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+          />
+          <Button onClick={capture}>Capture Photo</Button>
+        </>
+      ) : (
+        <>
+          <img src={image} alt="Captured" />
+          <Button onClick={retake}>Retake</Button>
+          <Button onClick={saveImage}>Submit</Button>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default CameraPage;
